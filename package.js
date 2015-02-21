47b715ca0a8f99a38abad209839b3d60b39decfc
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-02-21 17:43:52 +0100

    semantic-ui and colpick are move to the package

--- package.js
+++ package.js
@@ -12,6 +12,9 @@ Package.onUse(function(api) {
   api.use('templating', 'client');
   api.use('jquery', 'client');
   api.use('reactive-dict', 'client');
+  api.use('nooitaf:semantic-ui@1.9.2', 'client');
+  api.use('tommyjs:colpick@0.1.0', 'client');
+
   api.addFiles('text-editor.html', 'client');
   api.addFiles('text-editor.coffee', 'client');
   api.addFiles('text-editor.css', 'client');
